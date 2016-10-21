78f6aa01ffe464edfc25a866abe823e15c44264e
Michael Hung <michaelhung@shaw.ca>
2016-10-20 22:53:42 -0600

    Added test class

---
+++ cpsc501/src/ass2test/InspectorTest.java
@@ -0,0 +1,14 @@
+package ass2test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InspectorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
