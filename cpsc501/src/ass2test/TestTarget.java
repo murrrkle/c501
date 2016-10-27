98d0237a143b607eeed950d4aaf4689991d2b4b1
Michael Hung <michaelhung@shaw.ca>
2016-10-27 02:54:22 -0600

    Added/updated test classes

--- cpsc501/src/ass2test/TestTarget.java
+++ cpsc501/src/ass2test/TestTarget.java
@@ -1,12 +1,23 @@
 package ass2test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class TestTarget {
+public class TestTarget implements Iterable {
 	private String message;
+	private ArrayList<String> arr;
+	private TestTarget2 tt2;
+	private int[] iarr;
 
 	public TestTarget() {
 		message = "default";
+		arr = new ArrayList<String>();
+		arr.add("hello");
+		arr.add("World");
+		arr.add("!");
+		tt2 = new TestTarget2("gell");
+		iarr = new int[]{1, 2, 3, 4, 5, 6, 7};
 	}
 
 	public TestTarget(String text) {
@@ -24,5 +35,11 @@ public class TestTarget {
 	
 	public void display() throws IOException {
 		System.out.println("Goodbye cruel world");
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
---
+++ cpsc501/src/ass2test/TestTarget2.java
@@ -0,0 +1,15 @@
+package ass2test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TestTarget2 {
+	private String message;
+	private ArrayList<String> arr;
+
+	public TestTarget2(String text) {
+		message = text;
+
+	}
+}
