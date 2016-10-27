764fbd7cebe9c51193cb7fbd1d9b3fbbc4a94370
Michael Hung <michaelhung@shaw.ca>
2016-10-27 01:51:03 -0600

    Changed formatting with box drawing chars

--- cpsc501/src/ass2/Inspector.java
+++ cpsc501/src/ass2/Inspector.java
@@ -4,23 +4,24 @@ import java.lang.reflect.*;
 
 public class Inspector {
 	private Field[] fieldsToInspect;
-	
+
 	public void inspect(Object obj, boolean recursive) {
 		if (obj == null)
 			return;
-		
-		Class classObj = obj.getClass();
-		System.out.println("**********************************************");
-		
-		printInfo(classObj);
-		
-		if (recursive) {
 
-		} else {
-			
-		}
+		Class classObj = obj.getClass();
+
+		printInfo(classObj);
+
+		// if (recursive && fieldsToInspect.length != 0) {
+		// for (Field field : fieldsToInspect) {
+		// inspect(field.getType(), true);
+		// }
+		// } else {
+		// return;
+		// }
 	}
-	
+
 	private void printInfo(Class classObj) {
 		printClassName(classObj);
 		printInterfaces(classObj);
@@ -30,15 +31,19 @@ public class Inspector {
 	}
 
 	private void printClassName(Class classObj) {
+		System.out.println("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
+		System.out.println("│                 Class Name                 │");
+		System.out.println("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
 		System.out.println("Full Class Name: " + classObj.getName());
 		System.out.println("Class Name: " + classObj.getSimpleName());
+		System.out.println("Declaring Class Name: " + classObj.getDeclaringClass());
 		System.out.println("Immediate Superclass: " + classObj.getSuperclass().getName());
-
-		System.out.println();
-		System.out.println("**********************************************");
 	}
 
 	private void printInterfaces(Class classObj) {
+		System.out.println("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
+		System.out.println("│                 Interfaces                 │");
+		System.out.println("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
 
 		Class[] interfaces = classObj.getInterfaces();
 		System.out.println("Implemented Interfaces: ");
@@ -46,111 +51,114 @@ public class Inspector {
 			System.out.println("No implemented interfaces.");
 		}
 		for (Class i : interfaces) {
-			System.out.println("* " + i.getName() + " ");
+			System.out.println("> " + i.getName() + " ");
 		}
-
-		System.out.println();
-		System.out.println("**********************************************");
 	}
 
 	private void printFields(Class classObj) {
+		System.out.println("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
+		System.out.println("│                   Fields                   │");
+		System.out.println("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
+
 		fieldsToInspect = classObj.getDeclaredFields();
-		
+
 		System.out.println("Fields:");
-		
+
 		if (fieldsToInspect.length == 0) {
 			System.out.println("No fields.");
 		} else {
 			for (Field field : fieldsToInspect) {
-				System.out.println("* " + Modifier.toString(field.getModifiers()) + " " + field.getType().getSimpleName() + " " + field.getName() + " ");
+				System.out.println("> " + Modifier.toString(field.getModifiers()) + " "
+						+ field.getType().getSimpleName() + " " + field.getName() + " ");
+
 			}
 		}
-		
-		
-		System.out.println();
-		System.out.println("**********************************************");
 	}
 
 	private void printConstructorsInformation(Class classObj) {
+		System.out.println("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
+		System.out.println("│                Constructors                │");
+		System.out.println("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
 		Constructor[] constructors = classObj.getConstructors();
 		if (constructors.length == 0) {
-			System.out.println("No constructors.");
+			System.out.println("No Constructors.");
+			return;
 		}
 
 		for (Constructor constructor : constructors) {
-			System.out.println("Constructor name: " + constructor.getName());
+			System.out.println("Constructor Name: " + constructor.getName());
 
 			int modifiers = constructor.getModifiers();
-			System.out.println("Constructor modifier: " + Modifier.toString(modifiers));
+			System.out.println("Constructor Modifiers: " + Modifier.toString(modifiers));
 
 			Class[] parameterList = constructor.getParameterTypes();
-			System.out.println("Constructor parameter types: ");
+			System.out.println("Constructor Parameter Types: ");
 			if (parameterList.length == 0) {
 				System.out.println("No parameters.");
 			} else {
 				for (Class p : parameterList) {
-					System.out.println("* " + p.getName() + " ");
+					System.out.println("> " + p.getName() + " ");
 				}
 			}
 
 			System.out.println();
 
 			Class[] exceptionList = constructor.getExceptionTypes();
-			System.out.println("Exceptions thrown by constructor: ");
+			System.out.println("Thrown Exceptions: ");
 
 			if (exceptionList.length == 0) {
 				System.out.println("No thrown exceptions.");
 			} else {
 				for (Class e : exceptionList) {
-					System.out.println("* " + e.getName() + " ");
+					System.out.println("> " + e.getName() + " ");
 				}
 			}
-
-			System.out.println();
-			System.out.println("**********************************************");
-
+			System.out.println("\n==============================================\n");
 		}
 	}
 
 	private void printMethodInformation(Class classObj) {
+		System.out.println("┍━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┑");
+		System.out.println("│                   Methods                  │");
+		System.out.println("┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙");
 
 		Method[] methods = classObj.getDeclaredMethods();
 		if (methods.length == 0) {
 			System.out.println("No methods.");
+			return;
 		}
 		for (Method m : methods) {
-			System.out.println("method name: " + m.getName());
+			System.out.println("Method Name: " + m.getName());
 
-			System.out.println("Return type of method: " + m.getReturnType());
+			System.out.println("Method Return Type: " + m.getReturnType());
 
 			int modifiers = m.getModifiers();
-			System.out.println("method modifier: " + Modifier.toString(modifiers));
+			System.out.println("Method Modifiers: " + Modifier.toString(modifiers));
 
 			Class[] parameterList = m.getParameterTypes();
-			System.out.println("method parameter types: ");
+			System.out.println("Method Parameter Types: ");
 			if (parameterList.length == 0) {
 				System.out.println("No parameters.");
 			} else {
 				for (Class p : parameterList) {
-					System.out.println("* " + p.getName() + " ");
+					System.out.println("> " + p.getSimpleName() + " ");
 				}
 			}
 
 			System.out.println();
 
 			Class[] exceptionList = m.getExceptionTypes();
-			System.out.println("Exceptions thrown by method: ");
+			System.out.println("Thrown Exceptions: ");
 
 			if (exceptionList.length == 0) {
 				System.out.println("No thrown exceptions.");
 			} else {
 				for (Class e : exceptionList) {
-					System.out.println("* " + e.getName() + " ");
+					System.out.println("> " + e.getName() + " ");
 				}
 			}
 
-			System.out.println();
-			System.out.println("**********************************************");
+			System.out.println("\n==============================================\n");
 		}
 	}
 }
